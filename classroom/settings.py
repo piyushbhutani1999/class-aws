2cd8e8605a788077f481acf0f29af2ee8e683478
piyushbhutani1999 <45001688+piyushbhutani1999@users.noreply.github.com>
2019-08-24 03:16:11 +0530

    Update settings.py

--- classroom/settings.py
+++ classroom/settings.py
@@ -88,11 +88,14 @@ WSGI_APPLICATION = 'classroom.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'database_name',
+        'USER': 'postgres',
+        'PASSWORD': 'qwertypad',
+        'HOST': 'database-2.cowklmxtrnnv.ap-south-1.rds.amazonaws.com',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
